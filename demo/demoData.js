39a06e03fb8d3d30c9f1650684f037b50c87b5ec
nachiketha s h upadhya <nachikethashu@gmail.com>
2015-08-20 15:47:08 +0530

    The basic structure
    folder structures and basic files

---
+++ demo/demoData.js
@@ -0,0 +1,11 @@
+modelData = [{
+        src: "image1.png",
+        title: "image-one"
+    },{
+        src: "image2",
+        title: "image-two"
+    },{
+        src: "image3",
+        title: "image-three"
+    }
+];
---
+++ src/imageSlider.jquery.js
@@ -0,0 +1,9 @@
+(function( $ ) {
+
+    $.fn.imageSlider = function(data) {
+    	console.log(this);
+    	console.log(data);
+        return this;
+    };
+
+}( jQuery ));
